d18650668f659020e219af691c558404f3125849
Giezy Prado <giezyramirez27@gmail.com>
2023-11-03 21:06:39 -0600

    PRACTICA 03/11/2023 GEPR

--- src/app/formularios/datos-alumno/datos-alumno.component.ts
+++ src/app/formularios/datos-alumno/datos-alumno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-datos-alumno',
@@ -12,15 +12,19 @@ export class DatosAlumnoComponent implements OnInit {
 
   ngOnInit(): void{
     this.alumnos = new FormGroup({
-      matricula: new FormControl(''),
-      nombre: new FormControl(''),
-      apterno: new FormControl(''),
-      amaterno: new FormControl(''),
-      correo: new FormControl('')
-    })
+      matricula: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      nombre: new FormControl('',[Validators.required]),
+      apaterno: new FormControl('',[Validators.required]),
+      amaterno: new FormControl('',[Validators.required]),
+      correo: new FormControl('',[Validators.required]),
+    });
   }
 
   onSubmit(){
-
+    console.log(this.alumnos.controls['matricula'].value)
+    console.log(this.alumnos.controls['nombre'].value)
+    console.log(this.alumnos.controls['apaterno'].value)
+    console.log(this.alumnos.controls['amaterno'].value)
+    console.log(this.alumnos.controls['correo'].value)
   }
 }
